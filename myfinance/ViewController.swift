5be66300b440af6e60b6fcecffcf522f461b4238
Viktor Ban <viktorrichardban@gmail.com>
2024-02-09 21:06:08 +0100

    Initial commit

---
+++ myfinance/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myfinance
+//
+//  Created by Viktor Bán on 09/02/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
